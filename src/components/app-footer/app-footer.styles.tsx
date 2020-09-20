86ae67ac8a70282e37eee0f503c48a74c110d067
Martin <mr.mmaurer+work@gmail.com>
2020-09-20 01:56:12 +0100

    WIP

---
+++ src/components/app-footer/app-footer.styles.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'vue-styled-components';
+
+export const Test = styled.div`
+  display: block;
+  background: red;
+`;
--- src/components/app-footer/app-footer.tsx
+++ src/components/app-footer/app-footer.tsx
@@ -1,9 +1,25 @@
 import { defineComponent } from 'vue';
+import styled from 'vue-styled-components';
+// import * as Styles from './app-footer.styles';
+
+export const Test = styled.span`
+  display: block;
+  background: red;
+`;
 
 export default defineComponent({
   name: 'AppFooter',
+  components: {
+    // 'styled-test': Styles.Test,
+    // 'styled-test': Test2,
+    Test,
+  },
   props: {},
   setup() {
-    return () => <footer class="footer">AppFooter</footer>;
+    return () => (
+      <Test>
+        <footer class="footer">AppFooter</footer>
+      </Test>
+    );
   },
 });
