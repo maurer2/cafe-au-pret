2e8561cac01da192f197ab510e74f9487726630a
Martin <mr.mmaurer+work@gmail.com>
2020-09-22 23:22:44 +0100

    Added basic barcode

--- src/components/barcode/barcode.vue
+++ src/components/barcode/barcode.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="barcode">Barcode</div>
+  <div class="barcode">
+    <h2>Barcode</h2>
+    <svg
+      id="barcode"
+      ref="barcode"
+      jsbarcode-format="upc"
+      :jsbarcode-value="payload"
+      jsbarcode-textmargin="0"
+      jsbarcode-fontoptions="bold"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, Ref, ref, onMounted } from 'vue';
+  import JsBarcode from 'jsbarcode';
 
   export default defineComponent({
     name: 'Barcode',
+    components: {},
     props: {},
+    setup() {
+      const payload = '123456789012';
+      const barcodeDomElement: Ref<string | null> = ref('barcode');
+
+      onMounted(() => {
+        // JsBarcode(barcodeDomElement.value).init();
+        JsBarcode('#barcode').init();
+      });
+
+      return {
+        barcodeDomElement,
+        payload,
+      };
+    },
   });
 </script>
 
