260947cb1cc6870bac621756d33a120cbd53cb66
Martin <mr.mmaurer+work@gmail.com>
2020-09-27 23:15:07 +0100

    Added sanatize.css

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue';
 import App from './app.vue';
 import store from './store';
 
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
+
 createApp(App).use(store).mount('#app');
