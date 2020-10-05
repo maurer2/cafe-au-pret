6428c28564bbced548d640e64b1c78a2098caa03
Martin <mr.mmaurer+work@gmail.com>
2020-10-05 23:38:28 +0100

    Cleaned up qr code

--- src/components/qrcode/qrcode.tsx
+++ src/components/qrcode/qrcode.tsx
@@ -1,5 +1,5 @@
-import { defineComponent, Ref, ref, onMounted } from 'vue';
-import QRCodeGenerator from 'qrcode';
+import { defineComponent, Ref, ref, onMounted, computed } from 'vue';
+import QRCodeGenerator, { QRCodeToStringOptions } from 'qrcode';
 import styles from './qrcode.module.css';
 import { useStore } from '../../store';
 
@@ -9,50 +9,38 @@ export default defineComponent({
   props: {},
   setup() {
     const store = useStore();
+    const { userId } = store.state;
+    const zoomLevel = computed(() => store.state.zoomLevel.toFixed(2));
     const qrCodeMarkup: Ref<string | null> = ref(null);
-    const qrCodeImage: Ref<string | null> = ref(null);
 
     // SVG markup string
     function getQRCodeMarkup(payload: string): Promise<string> {
-      const qrCodeSettings = { errorCorrectionLevel: 'H', type: 'svg', margin: 1 };
-      const qrCodeString = QRCodeGenerator.toString(String(payload), qrCodeSettings);
+      const qrCodeSettings: QRCodeToStringOptions = {
+        errorCorrectionLevel: 'H',
+        type: 'svg',
+        margin: 0,
+      };
+      const qrCodeString = QRCodeGenerator.toString(payload, qrCodeSettings);
 
       return qrCodeString;
     }
 
-    // PNG base64 image
-    function getQRCodeImage(payload: string) {
-      const qrCodeSettings = { errorCorrectionLevel: 'H', width: '200', height: '200', margin: 0 };
-      const qrCodeString = QRCodeGenerator.toDataURL(String(payload), qrCodeSettings);
-
-      return qrCodeString;
+    function zoomIn() {
+      store.dispatch('increaseZoom');
     }
 
-    function handleClick(isZoomInButton = true) {
-      if (isZoomInButton) {
-        store.dispatch('increaseZoom');
-        return;
-      }
-
+    function zoomOut() {
       store.dispatch('decreaseZoom');
     }
+
+    function zoomReset() {}
 
     onMounted(() => {
       getQRCodeMarkup(store.state.userId)
         .then((markup: string) => {
           setTimeout(() => {
             qrCodeMarkup.value = markup;
-          }, 1000);
-        })
-        .catch((error: Error) => {
-          console.log(error);
-        });
-
-      getQRCodeImage(store.state.userId)
-        .then((markup: string) => {
-          setTimeout(() => {
-            qrCodeImage.value = markup;
-          }, 1000);
+          }, 1);
         })
         .catch((error: Error) => {
           console.log(error);
@@ -60,45 +48,27 @@ export default defineComponent({
     });
 
     return () => (
-      <div class={styles.component}>
-        <h2>QRCode (Zoom level {store.state.zoomLevel.toFixed(2)})</h2>
+      <div class={styles.qrcode}>
+        <h2>QRCode (Zoom level {zoomLevel.value})</h2>
+
         <>
-          {qrCodeMarkup.value === null && <div>Loading QR1</div>}
+          {qrCodeMarkup.value === null && <div>Loading QR code</div>}
           {qrCodeMarkup.value !== null && (
-            <figure class={styles.qrcode}>
-              <div class={styles.image}>
-                <div innerHTML={qrCodeMarkup.value} />
-              </div>
-              <figcaption>{store.state.userId}</figcaption>
+            <figure class={styles.qrcodePicture}>
+              <div class={styles.qrcodeImageWrapper} innerHTML={qrCodeMarkup.value}></div>
+              <figcaption class={styles.qrcodeText}>{userId}</figcaption>
             </figure>
           )}
         </>
 
-        <>
-          {qrCodeImage.value === null && <div>Loading QR2</div>}
-          {qrCodeImage.value !== null && (
-            <figure class={styles.qrcode}>
-              <img
-                class={styles.image}
-                src={qrCodeImage.value}
-                alt=""
-                style={{
-                  display: 'block',
-                  margin: `calc(1rem + ${store.state.zoomLevel}rem) auto`,
-                  transform: `scale(${store.state.zoomLevel})`,
-                  transformOrigin: 'center',
-                }}
-              />
-              <figcaption>{store.state.userId}</figcaption>
-            </figure>
-          )}
-        </>
-
-        <div class="button-group">
-          <button type="button" onClick={() => handleClick()}>
+        <div class={styles.controls}>
+          <button class={styles.control} type="button" onClick={() => zoomIn()}>
             Zoom in
           </button>
-          <button type="button" onClick={() => handleClick(false)}>
+          <button class={styles.control} type="button" onClick={() => zoomReset} disabled>
+            1x
+          </button>
+          <button class={styles.control} type="button" onClick={() => zoomOut()}>
             Zoom out
           </button>
         </div>
