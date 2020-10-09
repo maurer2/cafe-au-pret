4729dad290bc7a470203f3892895b8b472b2196b
Martin <mr.mmaurer+work@gmail.com>
2020-10-09 23:49:15 +0100

    Added menu import from json and basic styling

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,6 +1,7 @@
 import { defineComponent, ref, Ref, computed } from 'vue';
 import styles from './menu.module.css';
 import { useStore } from '../../store';
+import menuList from './menu.json';
 
 export default defineComponent({
   name: 'Menu',
@@ -8,14 +9,20 @@ export default defineComponent({
   props: {},
   setup() {
     const store = useStore();
-    const entries = [{ id: 'id', name: 'item' }];
+    const menuListSorted = [...menuList]; // todo
 
     return () => (
       <section class={styles.menu}>
         <h2>Menu</h2>
 
-        <ul>
-          <li>{entries[0].name}</li>
+        <ul class={styles.menuList}>
+          {menuListSorted.map((menuEntry) => (
+            <li class={styles.menuListEntry}>
+              <button class={styles.menuButton} type="button">
+                {menuEntry.name}
+              </button>
+            </li>
+          ))}
         </ul>
       </section>
     );
