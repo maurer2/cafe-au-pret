6904379eb658fb6083ba0c93a4e22a3e69a836ff
Martin <mr.mmaurer+work@gmail.com>
2020-11-07 01:44:43 +0000

    WIP progressbar attribute update

--- src/components/progress-bar/progress-bar.tsx
+++ src/components/progress-bar/progress-bar.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-debugger */
 import { defineComponent, computed, onMounted, ref, watch } from 'vue';
 import { nanoid } from 'nanoid';
 
@@ -15,8 +14,8 @@ export default defineComponent({
     const currentTime = computed(() => store.getters.getCurrentTime as string);
     const refreshTimeout = computed(() => store.state.refreshTimeoutInMinutes as number);
     const progressBarDomElement = ref(null as HTMLElement | null);
-    const animationKey = ref(`id-${nanoid(5)}`); // reset animation
-
+    const animationKey = ref(`key-${nanoid(5)}`); // reset animation
+    const progressBarValue = ref(0);
     // const currentAnimationState = ref('paused' as Properties<AnimationPlayState>);
     const currentAnimationState = ref('running' as 'paused' | 'running');
     const cssVars = computed(() => {
@@ -25,9 +24,10 @@ export default defineComponent({
         '--progress-animation-state': `${currentAnimationState.value}`,
       };
     });
+    let intervalId = -1;
 
     function resetAnimation() {
-      animationKey.value = `id-${nanoid(5)}`;
+      animationKey.value = `key-${nanoid(5)}`;
     }
 
     function startAnimation() {
@@ -38,15 +38,33 @@ export default defineComponent({
       currentAnimationState.value = 'running';
     }
 
+    function updateProgressBarValues() {
+      const timeoutInMS = refreshTimeout.value * 60 * 1000;
+      const parts = 10;
+      let index = 1;
+      progressBarValue.value = 0;
+
+      window.clearInterval(intervalId);
+
+      intervalId = window.setInterval(() => {
+        progressBarValue.value = index * parts;
+
+        index += 1;
+      }, timeoutInMS / parts);
+    }
+
     onMounted(() => {
       if (progressBarDomElement.value === null) {
         throw new Error('progressBarDomElement not found');
       }
+
+      updateProgressBarValues();
     });
 
     // temp watcher
     watch(currentTime, (newTime) => {
       console.log('update', newTime);
+      updateProgressBarValues();
       resetAnimation();
       startAnimation();
     });
@@ -57,11 +75,11 @@ export default defineComponent({
           ref={progressBarDomElement}
           class={styles.progressBarElement}
           max="100"
-          value="100"
+          value={progressBarValue.value}
           style={cssVars.value as any}
           key={animationKey.value}
         >
-          <span class="visually-hidden">50%</span>
+          <span class={styles.progressBarValue}>{progressBarValue.value}%</span>
         </progress>
       </section>
     );
