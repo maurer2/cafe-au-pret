e105a911c637705b6e1613d427e37022228a2ccf
Martin <mr.mmaurer+work@gmail.com>
2020-11-12 23:47:28 +0000

    Added isBlocked getter to header

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -9,12 +9,9 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const dateTimeKey = computed(() => store.getters.getCurrentDateKey as string);
-
     const remainingOrders = computed(() => store.getters.getDailyRemainingNumberOfOrders as number);
+    const isBlocked = computed(() => store.getters.isBlocked as boolean);
 
-    const isBlocked = { value: false };
-
-    // console.log(isBlocked);
     return () => (
       <header class={styles.header}>
         <h1 class={styles.title}>Coffeescript</h1>
@@ -22,8 +19,8 @@ export default defineComponent({
           <dt class={styles.statusTitle}>Remaining drinks:</dt>
           <dd class={styles.statusValue}>{remainingOrders.value}</dd>
 
-          <dt class={styles.statusTitle}>Status (Blocked):</dt>
-          <dd class={styles.statusValue}>{String(isBlocked.value)}</dd>
+          <dt class={styles.statusTitle}>Status:</dt>
+          <dd class={styles.statusValue}>{isBlocked.value ? 'Blocked' : 'Not Blocked'}</dd>
         </dl>
       </header>
     );
