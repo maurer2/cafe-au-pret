58fb4690dbc3141da3a88374bddca940ef5bbd60
Martin <mr.mmaurer+work@gmail.com>
2020-12-03 23:53:28 +0000

    WIP dragging

--- src/components/menu/menu-body-scrollable/menu-body-scrollable.tsx
+++ src/components/menu/menu-body-scrollable/menu-body-scrollable.tsx
@@ -25,57 +25,100 @@ export default defineComponent({
       // emit('add-drink', drink);
     }
 
+    const dragPositionX = ref(0);
     const scrollContainer = ref<HTMLElement | null>(null);
 
+    function updateDragging(event: PointerEvent) {
+      dragPositionX.value += event.movementX;
+
+      if (scrollContainer.value === null) {
+        return;
+      }
+
+      console.log('updatedragging', dragPositionX.value);
+
+      scrollContainer.value.scrollLeft = dragPositionX.value;
+    }
+
+    function startDragging(event: PointerEvent) {
+      if (scrollContainer.value === null) {
+        return;
+      }
+
+      scrollContainer.value.onpointermove = updateDragging;
+      scrollContainer.value.setPointerCapture(event.pointerId);
+    }
+
+    function stopDragging(event: PointerEvent) {
+      if (scrollContainer.value === null) {
+        return;
+      }
+
+      scrollContainer.value.onpointermove = null;
+      scrollContainer.value.releasePointerCapture(event.pointerId);
+    }
+
+    const scrollPositionX = computed(() => {
+      console.log(dragPositionX.value);
+      return `${dragPositionX.value}px`;
+    });
+
     return () => (
-      <div class={styles.menuListsContainer} ref={scrollContainer}>
-        <div class={styles.menuListContainer}>
-          <ul class={styles.menuList}>
-            {props.menuItems.map((menuEntry) => (
-              <li class={styles.menuListEntry}>
-                <button
-                  class={styles.menuButton}
-                  onClick={() => addDrink(menuEntry)}
-                  disabled={props.isBlocked}
-                  type="button"
-                >
-                  {menuEntry.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div class={styles.menuListContainer}>
-          <ul class={styles.menuList}>
-            {props.menuItems.map((menuEntry) => (
-              <li class={styles.menuListEntry}>
-                <button
-                  class={styles.menuButton}
-                  onClick={() => addDrink(menuEntry)}
-                  disabled={props.isBlocked}
-                  type="button"
-                >
-                  {menuEntry.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div class={styles.menuListContainer}>
-          <ul class={styles.menuList}>
-            {props.menuItems.map((menuEntry) => (
-              <li class={styles.menuListEntry}>
-                <button
-                  class={styles.menuButton}
-                  onClick={() => addDrink(menuEntry)}
-                  disabled={props.isBlocked}
-                  type="button"
-                >
-                  {menuEntry.name}
-                </button>
-              </li>
-            ))}
-          </ul>
+      <div class={styles.menuListsContainer}>
+        <div
+          class={styles.menuListsContainerInner}
+          ref={scrollContainer}
+          onPointerdown={startDragging}
+          onPointerup={stopDragging}
+        >
+          <div class={styles.menuListContainer}>
+            <ul class={styles.menuList}>
+              {props.menuItems.map((menuEntry) => (
+                <li class={styles.menuListEntry}>
+                  <button
+                    class={styles.menuButton}
+                    onClick={() => addDrink(menuEntry)}
+                    disabled={props.isBlocked}
+                    type="button"
+                  >
+                    {menuEntry.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div class={styles.menuListContainer}>
+            <ul class={styles.menuList}>
+              {props.menuItems.map((menuEntry) => (
+                <li class={styles.menuListEntry}>
+                  <button
+                    class={styles.menuButton}
+                    onClick={() => addDrink(menuEntry)}
+                    disabled={props.isBlocked}
+                    type="button"
+                  >
+                    {menuEntry.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div class={styles.menuListContainer}>
+            <ul class={styles.menuList}>
+              {props.menuItems.map((menuEntry) => (
+                <li class={styles.menuListEntry}>
+                  <button
+                    class={styles.menuButton}
+                    onClick={() => addDrink(menuEntry)}
+                    disabled={props.isBlocked}
+                    type="button"
+                  >
+                    {menuEntry.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     );
