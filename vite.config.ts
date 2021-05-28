3cbb7838dd2ff8ce934e36e8b99aeb431f0af750
Martin <mr.mmaurer+work@gmail.com>
2021-05-28 23:42:03 +0100

    Improved vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [vue(), vueJsx()],
+    };
+  }
+  return {
+    plugins: [vue(), vueJsx()],
+    build: {
+      target: ['chrome80', 'firefox60', 'safari13', 'edge18'],
+    },
+  };
 });
