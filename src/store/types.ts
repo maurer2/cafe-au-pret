23860087bf8a9ebe1de8a14f40c9fc9e8a9b8a34
Martin <mr.mmaurer+work@gmail.com>
2021-06-03 23:02:14 +0100

    Added new drink type for replacing enums

--- src/store/types.ts
+++ src/store/types.ts
@@ -51,6 +51,16 @@ export type GettersType = {
   [key: string]: (state: StateType, getters?: GettersType) => {};
 };
 
+export const drinks = [
+  'Coffee',
+  'Iced',
+  'Tea',
+  'Frappe',
+  'Smoothie',
+  'Other',
+] as const;
+export type Drinks = typeof drinks[number];
+
 export enum DrinkType {
   COFFEE = 'Coffee',
   ICED = 'Iced',
